40fa2b44181c75d6ccb6ba63c1d5dc6423519a6d
iholmes <iah6@cornell.edu>
2015-01-06 16:12:23 -0500

    test

---
+++ test.R
@@ -0,0 +1,13 @@
+# practice
+# install and load a new package
+install.packages("RSQLite", dependencies=TRUE)
+library(RSQLite)
+
+driver <- dbDriver("SQLite")
+conn <- dbConnect(driver, dbname="portal.mammals.sqlite")
+
+results <- dbGetQuery(conn, "SELECT species, wgt, year 
+                      FROM surveys")
+dbDisconnect(conn)
+
+
